e051cf6844afd962444dd11c2808541538945eee
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-08-26 20:27:20 +0300

    fix response errors

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -18,7 +18,7 @@ def create_id():
     if not match(
             r'^(?P<url>[a-z]+://[^\/\?:]+)(?P<port>:[0-9]+)?(?P<end>\/.*?)?$',
             data['url']):
-        raise InvalidAPIUsage('Недопустимый короткий идентификатор')
+        raise InvalidAPIUsage('Недопустимый короткий идентификатор', 400)
     if not data.get('custom_id'):
         data['custom_id'] = get_unique_short_id()
     if not match(
@@ -26,7 +26,7 @@ def create_id():
         raise InvalidAPIUsage(
             'Указано недопустимое имя для короткой ссылки', 400)
     if URL_map.query.filter_by(short=data['custom_id']).first():
-        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
+        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.', 400)
 
     url_map = URL_map()
     url_map.from_dict(data)
--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -5,10 +5,11 @@ from . import app, db
 
 class InvalidAPIUsage(Exception):
 
-    def __init__(self, message, status_code=400):
+    def __init__(self, message, status_code=None):
         super().__init__()
         self.message = message
-        self.status_code = status_code
+        if status_code is not None:
+            self.status_code = status_code
 
     def to_dict(self):
         return dict(message=self.message)
