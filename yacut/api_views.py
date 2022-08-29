5db4e022d8446aea05fe666d7223f2263fa49f68
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-08-29 17:47:22 +0300

    migrations,httpstatus

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from re import match
 
 from flask import jsonify, request
@@ -12,27 +13,27 @@ from .views import get_unique_short_id
 def create_id():
     data = request.get_json()
     if not data:
-        raise InvalidAPIUsage('Отсутствует тело запроса', 400)
+        raise InvalidAPIUsage('Отсутствует тело запроса')
     if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!', 400)
+        raise InvalidAPIUsage('"url" является обязательным полем!')
     if not match(
             r'^(?P<url>[a-z]+://[^\/\?:]+)(?P<port>:[0-9]+)?(?P<end>\/.*?)?$',
             data['url']):
-        raise InvalidAPIUsage('Недопустимый короткий идентификатор', 400)
+        raise InvalidAPIUsage('Недопустимый короткий идентификатор')
     if not data.get('custom_id'):
         data['custom_id'] = get_unique_short_id()
     if not match(
             r'^[A-Za-z0-9]{1,16}$', data['custom_id']):
         raise InvalidAPIUsage(
-            'Указано недопустимое имя для короткой ссылки', 400)
+            'Указано недопустимое имя для короткой ссылки')
     if URL_map.query.filter_by(short=data['custom_id']).first():
-        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.', 400)
+        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
 
     url_map = URL_map()
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify(url_map.to_dict()), 201
+    return jsonify(url_map.to_dict()), HTTPStatus.CREATED
 
 
 @app.route('/api/id/<string:short_id>/')
@@ -40,5 +41,5 @@ def get_url(short_id):
     url_map = URL_map.query.filter_by(short=short_id).first()
     if not url_map:
         raise InvalidAPIUsage(
-            'Указанный id не найден', 404)
+            'Указанный id не найден', HTTPStatus.NOT_FOUND)
     return jsonify({'url': url_map.original})
--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import jsonify, render_template
 
 from . import app, db
@@ -5,11 +7,10 @@ from . import app, db
 
 class InvalidAPIUsage(Exception):
 
-    def __init__(self, message, status_code=None):
+    def __init__(self, message, status_code=HTTPStatus.BAD_REQUEST):
         super().__init__()
         self.message = message
-        if status_code is not None:
-            self.status_code = status_code
+        self.status_code = status_code
 
     def to_dict(self):
         return dict(message=self.message)
@@ -22,10 +23,10 @@ def invalid_api_usage(error):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
